55e1041e6378402eb13788bc3dcd58eb372d4f1a
RmNLcN06 <romain.luciano@orange.fr>
2022-03-04 11:46:17 +0100

    Popup part in comment

--- js/script.js
+++ js/script.js
@@ -4,3 +4,12 @@ var menu = document.getElementsByClassName("header__top__menu")[0];
 menu.addEventListener("click", () => {
   menu.classList.toggle("change");
 });
+
+
+/* Pop-up Test
+  // When the user clicks on div, open the popup
+  function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+  }
+*/
