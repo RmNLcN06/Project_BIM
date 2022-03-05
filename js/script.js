f2e5e41a53e31f9615785541123dde2712c07755
RmNLcN06 <63708684+RmNLcN06@users.noreply.github.com>
2022-03-05 22:51:51 +0100

    Header Part Finish > Start Main Part

--- js/script.js
+++ js/script.js
@@ -1,15 +1,28 @@
 var menu = document.getElementsByClassName("header__top__menu")[0];
+var popupServices = document.getElementsByClassName(
+  "navigation__link--dropdown"
+)[0];
+var popupTools = document.getElementsByClassName(
+  "navigation__link--dropdown"
+)[1];
 
 // Menu Animation On Click
 menu.addEventListener("click", () => {
   menu.classList.toggle("change");
 });
 
+// Popup Services SubLinks On Click
+popupServices.addEventListener("click", () => {
+  var popupServicesLinks = document.getElementsByClassName(
+    "navigation__link--dropdown--sublink"
+  )[0];
+  popupServicesLinks.classList.toggle("show");
+});
 
-/* Pop-up Test
-  // When the user clicks on div, open the popup
-  function myFunction() {
-    var popup = document.getElementById("myPopup");
-    popup.classList.toggle("show");
-  }
-*/
+// Popup Tools SubLinks On Click
+popupTools.addEventListener("click", () => {
+  var popupToolsLinks = document.getElementsByClassName(
+    "navigation__link--dropdown--sublink"
+  )[1];
+  popupToolsLinks.classList.toggle("show");
+});
